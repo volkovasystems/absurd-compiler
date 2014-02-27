6404b8434402901bad4df9b15d72989e2be6b3b4
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-27 23:29:40 +0800

    Updated absurdCompiler procedure.

--- absurd-compiler.js
+++ absurd-compiler.js
@@ -6,19 +6,19 @@ try{ var base = window; }catch( error ){ base = exports; }
 		],
 		function construct( ){
 			var headNode = $( "head" );
-			headNode.append( styleNode );
+			headNode.ready( function onReady( ){
+				var absurdCompiler = function absurdCompiler( namespace, GUID ){
+					var styleNode = $( "<style></style>" );
+					styleNode.attr( "guid", GUID );
+					styleNode.attr( "namespace", namespace );
 
-			var absurdCompiler = function absurdCompiler( namespace, GUID ){
-				var styleNode = $( "<style></style>" );
-				styleNode.attr( "guid", GUID );
-				styleNode.attr( "namespace", namespace );
+					return ( function compiler( error, css ){
+						styleNode.html( css );
+						headNode.append( styleNode );
+					} );
+				};
 
-				return ( function compiler( error, css ){
-					styleNode.html( css );	
-				} );
-			};
-
-			base.absurdCompiler = absurdCompiler;
-			return absurdCompiler;
+				base.absurdCompiler = absurdCompiler;
+			} );
 		} );
 } )( base );
